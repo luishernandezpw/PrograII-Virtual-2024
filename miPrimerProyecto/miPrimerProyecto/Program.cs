a39912ca23b5bd638132d9672bb708df725a078a
Luis Hernandez <sarco_luis@hotmail.com>
2024-04-16 20:40:06 -0600

    Se creo el ejercicio de suma de dos numeros introducidos por el usuario

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -10,13 +10,18 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            //Ejercicio de sumar dos numeros.
-            int num1 = 5, num2 = 2, respuesta = 0;
+            //Ejercicio de sumar dos numeros introducidos por el usuario
+            int num1 = 0, num2 = 0, respuesta = 0;
+            Console.Write("Num 1: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            num2 = int.Parse(Console.ReadLine());
+
             respuesta = num1 + num2;
             Console.WriteLine("La suma es: "+ respuesta);//WriteLine permite imprimir en consola una cadena
                                                          // y da un salto de linea
             
-
             //Me permite hacer una pausa
             Console.ReadKey();
         }
