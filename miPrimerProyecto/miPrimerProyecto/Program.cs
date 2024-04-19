39f5d879bbe5ed8e01667e0e17980f405e1ebc18
Luis Hernandez <sarco_luis@hotmail.com>
2024-04-18 19:34:40 -0600

    Ejercicio de la tabla de multiplicar

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -11,26 +11,15 @@ namespace miPrimerProyecto
         static void Main(string[] args)
         {
             Boolean continuar = true;
-            while (continuar)
-            {
-                //clasificar la edad introducida por el usuario.
-                int edad = 0;
-
-                Console.Write("Dime tu edad: ");
-                edad = int.Parse(Console.ReadLine());
-
-                if (edad <= 2){
-                    Console.WriteLine("Eres un bebe");
-                } else if (edad < 12){
-                    Console.WriteLine("Eres un niño");
-                }else if (edad < 18){
-                    Console.WriteLine("Eres un adolescente");
-                }else if (edad <= 65) {
-                    Console.WriteLine("Eres un adulto");
-                }else{
-                    Console.WriteLine("Larga vida");
+            while (continuar) {
+                Console.Clear();//Limpia la consola
+                //Desarrollar el ejercicio de una tabla de multiplicar.
+                Console.Write("Tabla: ");
+                int tabla = int.Parse(Console.ReadLine());
+                for(int i=1; i<=10; i++) {
+                    Console.WriteLine("{0}x{1}={2}", tabla, i, tabla*i);
                 }
-                Console.Write("Desea continuar si cualquier tecla, salir presione la tecla 's': ");
+                Console.Write("Cualquier tecla para continuar, 's' para salir: ");
                 continuar = !Console.ReadLine().Equals("s");
             }
         }
