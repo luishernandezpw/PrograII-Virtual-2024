583a3afb3c820b80a5be42f320c839cbdc99616c
Luis Hernandez <sarco_luis@hotmail.com>
2024-04-18 19:18:52 -0600

    uso del estructura de control, del ciclo mientras.

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -10,25 +10,29 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            //clasificar la edad introducida por el usuario.
-            int edad = 0;
+            Boolean continuar = true;
+            while (continuar)
+            {
+                //clasificar la edad introducida por el usuario.
+                int edad = 0;
 
-            Console.Write("Dime tu edad: ");
-            edad = int.Parse(Console.ReadLine());
+                Console.Write("Dime tu edad: ");
+                edad = int.Parse(Console.ReadLine());
 
-            if( edad<=2 ){
-                Console.WriteLine("Eres un bebe");
-            }else if( edad<12){
-                Console.WriteLine("Eres un niño");
-            }else if( edad<18){
-                Console.WriteLine("Eres un adolescente");
-            }else if( edad<=65){
-                Console.WriteLine("Eres un adulto");
-            }else {
-                Console.WriteLine("Larga vida");
+                if (edad <= 2){
+                    Console.WriteLine("Eres un bebe");
+                } else if (edad < 12){
+                    Console.WriteLine("Eres un niño");
+                }else if (edad < 18){
+                    Console.WriteLine("Eres un adolescente");
+                }else if (edad <= 65) {
+                    Console.WriteLine("Eres un adulto");
+                }else{
+                    Console.WriteLine("Larga vida");
+                }
+                Console.Write("Desea continuar si cualquier tecla, salir presione la tecla 's': ");
+                continuar = !Console.ReadLine().Equals("s");
             }
-            //Me permite hacer una pausa
-            Console.ReadKey();
         }
     }
 }
