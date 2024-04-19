7c0792257c601cb2abbf36c49b0c7e3bcf0080e8
Luis Hernandez <sarco_luis@hotmail.com>
2024-04-18 19:48:11 -0600

    Ejercicio de los fibonacci

--- miPrimerProyecto/miPrimerProyecto/Program.cs
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -13,15 +13,19 @@ namespace miPrimerProyecto
             Boolean continuar = true;
             while (continuar) {
                 Console.Clear();//Limpia la consola
-                //Desarrollar el ejercicio de una tabla de multiplicar.
-                Console.Write("Tabla: ");
-                int tabla = int.Parse(Console.ReadLine());
-                for(int i=1; i<=10; i++) {
-                    Console.WriteLine("{0}x{1}={2}", tabla, i, tabla*i);
-                }
+                //Desarrollar el ejercicio de la secuencia fibonacci
+                int a = 1, b = 0, c = 0;
+                do {
+                    c = a + b;
+                    Console.WriteLine("{0}+{1}={2}", a, b, c);
+                    b = a;
+                    a = c;
+                }while (c < 1000);
+
                 Console.Write("Cualquier tecla para continuar, 's' para salir: ");
                 continuar = !Console.ReadLine().Equals("s");
             }
         }
+
     }
 }
