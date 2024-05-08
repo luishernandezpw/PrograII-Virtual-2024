be9ebb9d2cc4ba81e8f4fc8846f14a28de411c34
Luis Hernandez <sarco_luis@hotmail.com>
2024-05-08 02:01:39 +0000

    Creacion de un ejercicio en monoDevelop en Linux

---
+++ holaMundo.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class holaMundo{
+    public static void Main(string[] args){
+        Console.Write("Num 1: ");
+        double num1 = double.Parse(Console.ReadLine());
+        
+        Console.Write("Num 2: ");
+        double num2 = double.Parse(Console.ReadLine());
+        
+        double resp = num1 + num2;
+
+        Console.WriteLine("La Suma es: {0}", resp);
+    }
+}
